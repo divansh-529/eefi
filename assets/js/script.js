e627d68a6a1e6b00d7afc5fe8c44795dd9e73685
divansh-529 <divanshsachdev@gmail.com>
2024-09-30 11:19:14 +0530

    back to top add sucess

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,23 @@ menuicon.addEventListener("click", function () {
     menubarThird.classList.toggle("-rotate-[40deg]");
     body.classList.toggle("overflow-lg-hidden")
 })
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
